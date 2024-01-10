2d0a99bc6d5461265ef6664937014fc9633f2b23
柞磨智章 <88645787+tomtaylor24@users.noreply.github.com>
2024-01-10 09:44:04 +0900

    動画フィードバック修正

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
 import "./news.css";
-import posts from './data/posts'
+import News from './pages/news'
 
-const News = ({ props }) => {
+const App = () => {
   return (
-    <ul className="newsList">
-      {posts.map((elem) => (
-        <li className="newsItem">
-          <a href="">
-          <div className="newsTop">
-            <p className="newsDate">{elem.createdAt.split("T")[0]}</p>
-            <div className="newsLabelWrap">
-              {elem.categories.map((category) => (
-                <p className="newsLabel">{category}</p>
-              ))}
-            </div>
-          </div>
-          <h2 className="newsTitle">{elem.title}</h2>
-          <p className="newsContent">{elem.content}</p>
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <News />
+    </div>
   );
 }
 
-export default News;
+export default App;
