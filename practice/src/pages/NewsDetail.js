9c9a75cc558a7e54af8012c961c8c75e5bc0df49
柞磨智章 <88645787+tomtaylor24@users.noreply.github.com>
2024-01-17 11:27:41 +0900

    task03フィードバック修正

--- practice/src/pages/NewsDetail.js
+++ practice/src/pages/NewsDetail.js
@@ -1,20 +1,29 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 // import posts from "../data/posts";
 
 const NewsDetail = () => {
-  const [newsData, setNewsData] = useState();
+  const [newsData, setNewsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
-  
+
   useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`);
+      const res = await fetch(
+        `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`
+      );
       const data = await res.json();
       setNewsData(data.post);
+      setIsLoading(false);
     };
     fetcher();
   }, [id]);
-  if (!newsData) return; 
+  if (isLoading) {
+    return <div>読み込み中…</div>;
+  }
+  if (!newsData) {
+    return <div>記事が見つかりません</div>;
+  }
 
   return (
     <div className="newsDetail inner">
